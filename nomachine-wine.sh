6c37bc0a39f298ea4113342d666274d1d238b676
Leonardo28l1 <lipao6911@gmail.com>
2024-12-12 15:43:34 -0300

    Update nomachine-wine.sh

--- nomachine-wine.sh
+++ nomachine-wine.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/Leonardo28l1/Docker-Ubuntu-Desktop-NoMachine/raw/refs/heads/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
